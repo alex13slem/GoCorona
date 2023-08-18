db4b330b4ea494441d600613a760fb0d1a0f4dad
alex13slem <a.s.scherba@yandex.by>
2023-08-19 00:55:26 +0300

    foxed errors

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,7 +1,4 @@
 export * from './useDOMRectHeight';
-export * from './useDocumentFavicon';
 export * from './useDocumentTitle';
-// export * from './useFetching';
 export * from './useScrollElemHide';
 export * from './useScrollElemOpacity';
-export * from './useSelectorStyle';
